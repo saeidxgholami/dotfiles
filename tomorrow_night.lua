546451bb0bddb2ae6f70ca5a58e239e1eb336299
Saeid Gholami <75513762+saeidxgholami@users.noreply.github.com>
2021-02-13 18:55:48 +0330

    Create tomorrow_night.lua

---
+++ tomorrow_night.lua
@@ -0,0 +1,30 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#1d1f21" }
+style.background2 = { common.color "#1d1f21" }
+style.background3 = { common.color "#1d1f21" }
+style.text = { common.color "#c5c8c6" }
+style.caret = { common.color "#b294bb" }
+style.accent = { common.color "#ffffff" }
+style.dim = { common.color "#4f5873" }
+style.divider = { common.color "#181A1F" }
+style.selection = { common.color "#373b41" }
+style.line_number = { common.color "#53576e" }
+style.line_number2 = { common.color "#666B76" }
+style.line_highlight = { common.color "#282a2e" }
+style.scrollbar = { common.color "#353B45" }
+style.scrollbar2 = { common.color "#353B45" }
+
+style.syntax["normal"] = { common.color "#c5c8c6" }
+style.syntax["symbol"] = { common.color "#c5c8c6" }
+style.syntax["comment"] = { common.color "#969896" }
+style.syntax["keyword"] = { common.color "#b294bb" }
+style.syntax["keyword2"] = { common.color "#de935f" }
+style.syntax["number"] = { common.color "#de935f" }
+style.syntax["literal"] = { common.color "#f0c674" }
+style.syntax["string"] = { common.color "#b5bd68" }
+style.syntax["operator"] = { common.color "#c5c8c6" }
+style.syntax["function"] = { common.color "#81a2be" }
+-- style.syntax["function"] = { common.color "#8abeb7" }
+
